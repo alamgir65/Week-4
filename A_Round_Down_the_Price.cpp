e4c5210f05b9d0d4170aaeaa5176b4a7b1c0e17f
alamgir65 <122733174+alamgir65@users.noreply.github.com>
2023-10-24 23:51:36 +0600

    Add files via upload

---
+++ A_Round_Down_the_Price.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int digit=0;
+        int m=n;
+        while(n>0){
+            digit++;
+            n=n/10;
+        }
+        digit--;
+        int x=1;
+        while(digit--){
+            x=x*10;
+        }
+        cout<<(m-x)<<endl;
+    }
+    return 0;
+}
